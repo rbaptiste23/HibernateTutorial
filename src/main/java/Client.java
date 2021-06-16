6fc04ceff5be5bffb3d5ff0fa77e08af1b3b2360
Riley Baptiste <RILEYB@BIRLASOFT.COM>
2021-06-16 16:35:40 -0500

    First commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,37 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+
+        EntityManagerFactory emf =  Persistence.createEntityManagerFactory("org.hibernate.tutorial.jpa");
+        EntityManager em =  emf.createEntityManager();
+        EntityTransaction et = em.getTransaction();
+
+        try {
+            et.begin();
+
+
+
+
+            et.commit();
+
+
+        } catch (Exception e) {
+            et.rollback();
+            System.out.println(e);
+
+        } finally {
+            em.close();
+            emf.close();
+        }
+
+    }
+
+
+}
+
